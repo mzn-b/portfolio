53a3f2b68a3c974c920f11b2e52921aaf31b79f3
mzn-b <medomedso300@gmail.com>
2024-03-26 22:52:59 +0100

    fix navbar

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC} from "react";
 
 interface LayoutProps {
   setActivePage: React.Dispatch<React.SetStateAction<number>>;
@@ -9,7 +9,7 @@ export const Layout: FC<LayoutProps> = ({ setActivePage, children }) => {
   return (
     <>
       <Navbar setActivePage={setActivePage} />
-      {children}
+      <div className="pt-16">{children}</div>
     </>
   );
 };
@@ -19,62 +19,43 @@ interface NavbarProps {
 }
 
 const Navbar: FC<NavbarProps> = ({ setActivePage }) => {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.scrollY;
-      if (scrollTop > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
-    <nav className={`py-4 ${isScrolled ? 'bg-navbar-gradient' : 'bg-transparent'} fixed top-0 left-0 w-full z-50`}>
+    <nav className={`py-4 bg-navbar-gradient fixed top-0 left-0 w-full z-50`}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center">
-          <div className="hidden sm:block sm:ml-6">
-            <div className="flex space-x-4">
-              {/* Navigation links */}
-              <a
-                onClick={() => {
-                  setActivePage(1);
-                }}
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-m font-medium"
-              >
-                Home
-              </a>
-              <a
-                onClick={() => {
-                  setActivePage(2);
-                }}
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-m font-medium"
-              >
-                About
-              </a>
-              <a
-                onClick={() => {
-                  setActivePage(2);
-                }}
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-m font-medium"
-              >
-                Services
-              </a>
-              <a
-                onClick={() => {
-                  setActivePage(2);
-                }}
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-m font-medium"
-              >
-                Contact
-              </a>
-            </div>
+          <div className="flex space-x-4">
+            <a
+              onClick={() => {
+                setActivePage(1);
+              }}
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-m font-medium"
+            >
+              Home
+            </a>
+            <a
+              onClick={() => {
+                setActivePage(2);
+              }}
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-m font-medium"
+            >
+              About
+            </a>
+            <a
+              onClick={() => {
+                setActivePage(3);
+              }}
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-m font-medium"
+            >
+              Services
+            </a>
+            <a
+              onClick={() => {
+                setActivePage(4);
+              }}
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-m font-medium"
+            >
+              Contact
+            </a>
           </div>
         </div>
       </div>
